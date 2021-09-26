2ef6ec9afa4e456f9070a8dcaf3a9a6f75fffabe
Henrique <ricci.henrique@outlook.com>
2021-09-26 20:46:00 -0300

    fix socket vs http

--- index.js
+++ index.js
@@ -8,41 +8,50 @@ const usersOnline = {};
 const net = require('net');
 
 const server = net.createServer((conn) => {
-  console.log('Cliente conectado');
-
-  conn.on('end', () => {
-    console.log('Cliente desconectado');
-  });
-
-  conn.on('data', (data) => {
-        data = JSON.parse(data);
-        const { type } = data;
+    console.log('Cliente conectado');
     
+    conn.on('end', () => {
+        console.log('Cliente desconectado');
+    });
+    
+    conn.on('data', (data) => { 
+        try {
+            data = JSON.parse(data);
+        }
+        catch(ex) {
+            let req = data.toString().split('\n')[0];
+
+            if (req.startsWith('GET')) {
+                req = req.substring(6).split(' ')[0].replace(/&/g, '=').split('=');
+                data = {};
+                for(let i = 0; i < req.length; i += 2) {
+                    data[req[i]] = req[i + 1];
+                }
+                data.type = 'cmd';
+                console.log(data);
+            }
+            else {
+                throw {
+                    error: 'request inválido',
+                }
+            }            
+        }
+
+        const { type } = data;
+        
         switch (type) {
             case 'name':
                 const { name } = data;
                 usersOnline[name] = { conn };
             break;
+            case 'cmd':
+                const { user, cmd } = data;
+                usersOnline[user] && usersOnline[user].conn.write(JSON.stringify({ type: 'cmd', cmd }));
+            break;
         }
-  });
+    });
 });
 
-
-app.use(bodyParser.json());
-
-app.get('/login');
-
-app.get('/ping', (_, res) => {
-    res.end();
-});
-
-app.post('/exec', (req, res) => {
-    console.log('eae');
-    const { cmd, user } = req.body;
-    usersOnline[user] && usersOnline[user].conn.write(JSON.stringify({ type: 'cmd', cmd }));
-    
-    res.end();
-});
 server.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor escutando na porta 8080');
+    console.log('Servidor escutando na porta 8080');
 });
